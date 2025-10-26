83df07b25f632287088f7fa43b87a26cd7b1570b
Sophie Lin <98501518+soph-lin@users.noreply.github.com>
2025-10-26 14:52:58 -0500

    Initial Commit

---
+++ vacation-review/vacation_reviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  vacation_reviewApp.swift
+//  vacation-review
+//
+//  Created by Sophie Lin on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct vacation_reviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
